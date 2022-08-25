4ce498c9f4fac12a46c3363d73c7cb0dcc8b982d
Alec Trievel <atrievel@gmail.com>
2022-08-25 16:11:12 -0400

    Modify test setup and scripts

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,16 @@ const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "node",
   verbose: true,
-  automock: true,
-}
+  globals: {
+    "ts-jest": {
+      isolatedModules: false,
+    },
+  },
+  roots: ["<rootDir>"],
+  moduleNameMapper: {
+    "@algorithms/(.*)": "<rootDir>/src/algorithms/$1",
+    "@contracts/(.*)": "<rootDir>/src/contracts/$1",
+    "@utils/(.*)": "<rootDir>/src/utils/$1",
+  },
+};
 export default config;
