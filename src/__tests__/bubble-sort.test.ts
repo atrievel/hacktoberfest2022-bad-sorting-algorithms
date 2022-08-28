b9f7e3f25696c61981e0ff8837104816b9accf08
Alec Trievel <atrievel@gmail.com>
2022-08-28 16:22:44 -0400

    Add bubble sort test

---
+++ src/__tests__/bubble-sort.test.ts
@@ -0,0 +1,58 @@
+import BubbleSort from "@algorithms/bubble-sort";
+
+const sortedNumbers: readonly number[] = [1, 2, 5, 21, 58, 1234];
+const sortedStrings: string[] = ["A", "list", "of", "strings"];
+
+describe("Bubble Sort Test Suite", () => {
+  let randomOrderNumbers: number[] = [];
+  let randomOrderStrings: string[] = [];
+
+  beforeAll(() => {
+    randomOrderNumbers = [...sortedNumbers].sort(() => Math.random() - 0.5);
+    randomOrderStrings = [...sortedStrings].sort(() => Math.random() - 0.5);
+  });
+
+  it("sorts numbers in asc order by default", () => {
+    // Arrange
+    const uut = new BubbleSort(randomOrderNumbers);
+
+    // Act
+    const sorted = uut.sort();
+
+    // Assert
+    expect(sorted).toEqual(sortedNumbers);
+  });
+
+  it("sorts numbers in desc order", () => {
+    // Arrange
+    const uut = new BubbleSort(randomOrderNumbers, true);
+
+    // Act
+    const sorted = uut.sort().reverse();
+
+    // Assert
+    expect(sorted).toEqual(sortedNumbers);
+  });
+
+  it("sorts strings in asc order by default", () => {
+    // Arrange
+    const uut = new BubbleSort(randomOrderStrings);
+
+    // Act
+    const sorted = uut.sort();
+
+    // Assert
+    expect(sorted).toEqual(sortedStrings);
+  });
+
+  it("sorts strings in desc order", () => {
+    // Arrange
+    const uut = new BubbleSort(randomOrderStrings, true);
+
+    // Act
+    const sorted = uut.sort().reverse();
+
+    // Assert
+    expect(sorted).toEqual(sortedStrings);
+  });
+});
