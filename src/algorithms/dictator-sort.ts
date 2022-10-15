dae4110552cce4e8b16d7a7a88eda55de8ca36a5
josef <macerajosef@gmail.com>
2022-10-15 14:56:01 -0700

    added dictator sort and corresponding test

--- src/algorithms/dictator-sort.ts
+++ src/algorithms/dictator-sort.ts
@@ -10,13 +10,15 @@ export default class DictatorSort<
   sort(): T[] {
     const sortedCollection = [...this.collectionToSort];
     for (let i = 1; i < this.collectionToSort.length; i++) {
+      // for each element in the collection
       if (sortedCollection[i] < sortedCollection[i - 1]) {
-        // remove the current one
+        // if the current element is not sorted with respect to how far we've reversed the array, remove the current one
         sortedCollection.splice(i, 1);
+        // decrement i because we removed an element and the index length changes
         i--;
       }
     }
 
-    return this.sortDesc ? sortedCollection.reverse() : sortedCollection;
+    return this.sortDesc ? sortedCollection.reverse() : sortedCollection; // reverse if the constructor specified so.
   }
 }
